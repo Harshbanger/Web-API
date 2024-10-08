b94f5d3897423441105b1ef93b1dd33a11cf1f01
Harshbanger <131772882+Harshbanger@users.noreply.github.com>
2024-10-08 12:43:53 +0200

    web app

---
+++ session4/Viewer/webApp/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { FriendsComponent } from './friends/friends.component';
+import { ProfileComponent } from './profile/profile.component';
+
+export const routes: Routes = [
+    {path:"/" , component:HomeComponent},
+    {path:"friends" , component:FriendsComponent},
+    {path:"/profile" , component:ProfileComponent},
+];
