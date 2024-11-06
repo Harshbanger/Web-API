a57d8c0739f4c7a3704d4038f26e6638c147ce0d
Harshbanger <131772882+Harshbanger@users.noreply.github.com>
2024-11-06 13:56:23 +0100

    7

--- session7/routes/students.js
+++ session7/routes/students.js
@@ -1,7 +1,7 @@
 // routes/students.js
 const express = require('express');
 const router = express.Router();
-const students = require('../data/students.json');
+const students = require('../data/student.json');
 
 router.get('/', (req, res) => {
   res.json(students);
